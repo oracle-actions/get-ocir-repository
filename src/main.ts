a6aaaa91d8949abe4317ef58e09121b8f00327e1
Avi Miller <avi.miller@oracle.com>
2024-06-20 12:35:22 +1000

    chore: update action runtime to node20
    Signed-off-by: Avi Miller <avi.miller@oracle.com>

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,61 @@
-/* Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+/* Copyright (c) 2021, 2024, Oracle and/or its affiliates.
  * Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
  */
 
-import * as core from '@actions/core';
-import * as artifacts from 'oci-artifacts';
-import * as identity from 'oci-identity';
-import { Region, SimpleAuthenticationDetailsProvider } from 'oci-common';
+import * as core from '@actions/core'
+import * as artifacts from 'oci-artifacts'
+import * as identity from 'oci-identity'
+import { Region, SimpleAuthenticationDetailsProvider } from 'oci-common'
 
-async function getOcirRepo(): Promise<void> {
-  // Required environment variables
-  const tenancy = process.env.OCI_CLI_TENANCY || '';
-  const user = process.env.OCI_CLI_USER || '';
-  const fingerprint = process.env.OCI_CLI_FINGERPRINT || '';
-  const privateKey = process.env.OCI_CLI_KEY_CONTENT || '';
-  const region = Region.fromRegionId(process.env.OCI_CLI_REGION || '');
+export async function getOcirRepo(): Promise<void> {
+  try {
+    // Required environment variables
+    const tenancy = process.env.OCI_CLI_TENANCY || ''
+    const user = process.env.OCI_CLI_USER || ''
+    const fingerprint = process.env.OCI_CLI_FINGERPRINT || ''
+    const privateKey = process.env.OCI_CLI_KEY_CONTENT || ''
+    const region = Region.fromRegionId(process.env.OCI_CLI_REGION || '')
 
-  const authProvider = new SimpleAuthenticationDetailsProvider(
-    tenancy,
-    user,
-    fingerprint,
-    privateKey,
-    null,
-    region
-  );
+    const authProvider = new SimpleAuthenticationDetailsProvider(tenancy, user, fingerprint, privateKey, null, region)
 
-  const ac = new artifacts.ArtifactsClient({authenticationDetailsProvider: authProvider});
-  const ic = new identity.IdentityClient({authenticationDetailsProvider: authProvider});
+    const ac = new artifacts.ArtifactsClient({ authenticationDetailsProvider: authProvider })
+    const ic = new identity.IdentityClient({ authenticationDetailsProvider: authProvider })
 
-  const compartmentId: string = core.getInput('compartment', {required: true});
-  const displayName: string = core.getInput('name', {required: true});
+    const compartmentId: string = core.getInput('compartment', { required: true })
+    const displayName: string = core.getInput('name', { required: true })
 
-  const namespace = (await ac.getContainerConfiguration({compartmentId: compartmentId})).containerConfiguration
-    .namespace;
-  const regionCode = (await ic.listRegions({})).items
-    .find(x => x.name === authProvider.getRegion().regionId)
-    ?.key?.toLocaleLowerCase();
-  const ocir = regionCode ? `${regionCode}.ocir.io` : '';
+    const namespace = (await ac.getContainerConfiguration({ compartmentId })).containerConfiguration.namespace
+    const regionCode = (await ic.listRegions({})).items
+      .find(x => x.name === authProvider.getRegion().regionId)
+      ?.key?.toLocaleLowerCase()
+    const ocir = regionCode ? `${regionCode}.ocir.io` : ''
 
-  if (ocir) {
-    const repo = (
-      await ac.listContainerRepositories({compartmentId: compartmentId})
-    ).containerRepositoryCollection.items.find(x => x.displayName === displayName);
+    if (ocir) {
+      const repo = (await ac.listContainerRepositories({ compartmentId })).containerRepositoryCollection.items.find(
+        x => x.displayName === displayName
+      )
 
-    if (repo) {
-      core.setOutput('repo_path', `${ocir}/${namespace}/${repo.displayName}`);
-      core.setOutput('repo_ocid', `${repo.id}`);
+      if (repo) {
+        core.setOutput('repo_path', `${ocir}/${namespace}/${repo.displayName}`)
+        core.setOutput('repo_ocid', `${repo.id}`)
+      } else {
+        const containerRepository = (
+          await ac.createContainerRepository({
+            createContainerRepositoryDetails: {
+              compartmentId,
+              displayName,
+              isImmutable: false,
+              isPublic: false
+            }
+          })
+        ).containerRepository
+        core.setOutput('repo_path', `${ocir}/${namespace}/${containerRepository.displayName}`)
+        core.setOutput('repo_ocid', `${containerRepository.id}`)
+      }
     } else {
-      const containerRepository = (
-        await ac.createContainerRepository({
-          createContainerRepositoryDetails: {
-            compartmentId: compartmentId,
-            displayName: displayName,
-            isImmutable: false,
-            isPublic: false
-          }
-        })
-      ).containerRepository;
-      core.setOutput('repo_path', `${ocir}/${namespace}/${containerRepository.displayName}`);
-      core.setOutput('repo_ocid', `${containerRepository.id}`);
+      core.setFailed('Failed to identify OCIR endpoint.')
     }
-  } else {
-    core.setFailed('Failed to identify OCIR endpoint.');
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
-
-getOcirRepo().catch(e => {
-  if (e instanceof Error) core.setFailed(e.message);
-});
